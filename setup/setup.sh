5bce87339a60e150ca0796e306142be5ff0821e2
Ka1kai <hal.game.mail@gmail.com>
2016-10-12 15:57:46 +0900

    setup shell追加

---
+++ setup/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# シンボリックリンクを貼る
+cd ~/
+
+ln -s -f ~/dotfiles/.bash_profile
+ln -s -f ~/dotfiles/.bashrc
+ln -s -f ~/dotfiles/.ctags
+ln -s -f ~/dotfiles/.gitconfig
+ln -s -f ~/dotfiles/.gitignore
+ln -s -f ~/dotfiles/.netrc
+ln -s -f ~/dotfiles/.tmux.conf
+ln -s -f ~/dotfiles/.vimrc
+ln -s -f ~/dotfiles/.zshrc
